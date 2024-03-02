63997f5fe08807cea02a5cb81e332af7ad08700b
Matthew Dutton <dutton.matt@gmail.com>
2024-03-02 14:18:36 +0000

    Added command line args

--- node/pb-monitor.go
+++ node/pb-monitor.go
@@ -1,1 +1,33 @@
 package main
+
+import(
+	"fmt"
+	"os"
+)
+
+// Parse command line args for 
+//	interface to use 
+//	storage buffer size 
+//	try to detect MAC randomisation 
+//	specific SSID
+
+func main()  {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += s + os.Args[i]
+		s = " "
+	}
+	fmt.Println(s)
+
+}	
+
+
+# Set up connection to local interface
+
+# Capture packets
+
+# Filter out non-beacon or non-broadcast or SSID matches
+
+# Parse frame
+
+# Store up to buffer size?
