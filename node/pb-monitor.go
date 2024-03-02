609ff6b84cf0ea9b90bb70565191a8866c94a36f
Matthew Dutton <dutton.matt@gmail.com>
2024-03-02 14:19:23 +0000

    Commit 1

--- node/pb-monitor.go
+++ node/pb-monitor.go
@@ -1,33 +1,42 @@
 package main
 
-import(
+import (
 	"fmt"
+	"log"
 	"os"
 )
 
-// Parse command line args for 
-//	interface to use 
-//	storage buffer size 
-//	try to detect MAC randomisation 
+// Parse command line args for
+//	interface to use
+//	storage buffer size
+//	try to detect MAC randomisation
 //	specific SSID
 
-func main()  {
+func main() {
+
+	var usage string = "USAGE: pb-monitor.go <interface name> <buffer entries>"
+
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
-		s += s + os.Args[i]
-		s = " "
+		s += sep + os.Args[i]
+		sep = " "
 	}
+
+	if len(os.Args) != 3 {
+		fmt.Printf("%s \n", usage)
+		log.Fatal("Incorrect number of command line arguments.")
+	}
+
 	fmt.Println(s)
 
-}	
+}
 
+// Set up connection to local interface
 
-# Set up connection to local interface
+// Capture packets
 
-# Capture packets
+// Filter out non-beacon or non-broadcast or SSID matches
 
-# Filter out non-beacon or non-broadcast or SSID matches
+// Parse frame
 
-# Parse frame
-
-# Store up to buffer size?
+// Store up to buffer size?
