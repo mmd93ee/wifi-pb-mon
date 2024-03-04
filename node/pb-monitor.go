8c3b44f8bb3141f3b49e5ddafd221116a71b227c
Matthew Dutton <dutton.matt@gmail.com>
2024-03-04 19:42:58 +0000

    Updated to use pcap

--- node/pb-monitor.go
+++ node/pb-monitor.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 )
 
 var (
-	usage      string = "USAGE: pb-monitor.go <interface name> <buffer entries>"
-	iface             = flag.String("i", "wlan1", "Interface name to use")
-	pbuffer           = flag.Int("b", 1000, "Maximum queue size for packet decode")
-	rDetect           = flag.Bool("r", false, "Counter MAC Randomisation")
-	filterSSID        = flag.String("s", "all", "Restrict to a single SSID (network or node)")
+
+	// Global variables
+	timeout time.Duration = 30 * time.Second
+
+	// Command line variables
+	iface      string
+	pBuffer    int
+	rDetect    bool
+	filterSSID string
 )
 
 func main() {
 
+	// Set flags
+	flag.StringVar(&iface, "i", "lo1", "Interface name to use")
+	flag.IntVar(&pBuffer, "b", 1000, "Maximum queue size for packet decode")
+	flag.BoolVar(&rDetect, "r", false, "Counter MAC Randomisation")
+	flag.StringVar(&filterSSID, "s", "all", "SSID Filter")
+	flag.Parse()
+	flag.PrintDefaults()
+
+	createPacketSource()
 }
 
 // Set up connection to local interface
-func createPacketSource PacketSource {}
+func createPacketSource() {
 
+	handle, err := pcap.OpenLive(iface, 65536, true, time.Duration(timeout.Seconds()))
+	if err != nil {
+		panic(err)
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Printf("Packet: %v", packet) // Do something with a packet here.
+	}
+}
 
 // Capture packets
 
