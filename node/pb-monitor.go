225c953bbddcbfb5f97d39ddf7650ddf31370902
mmd93ee <dutton.matt@gmail.com>
2024-03-04 15:59:17 +0000

    Updating.

--- node/pb-monitor.go
+++ node/pb-monitor.go
@@ -1,37 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+	"flag"
 )
 
-// Parse command line args for
-//	interface to use
-//	storage buffer size
-//	try to detect MAC randomisation
-//	specific SSID
+var (
+	usage      string = "USAGE: pb-monitor.go <interface name> <buffer entries>"
+	iface             = flag.String("i", "wlan1", "Interface name to use")
+	pbuffer           = flag.Int("b", 1000, "Maximum queue size for packet decode")
+	rDetect           = flag.Bool("r", false, "Counter MAC Randomisation")
+	filterSSID        = flag.String("s", "all", "Restrict to a single SSID (network or node)")
+)
 
 func main() {
-
-	var usage string = "USAGE: pb-monitor.go <interface name> <buffer entries>"
-
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-
-	if len(os.Args) != 3 {
-		fmt.Printf("%s \n", usage)
-		log.Fatal("Incorrect number of command line arguments.")
-	}
-
-	fmt.Println(s)
 
 }
 
 // Set up connection to local interface
+func createPacketSource PacketSource {}
+
 
 // Capture packets
 
