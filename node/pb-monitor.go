790f9df399c900a80ac3103c86e30bd2be001e58
mmd93ee <dutton.matt@gmail.com>
2024-03-24 17:12:51 +0000

    Created Probe debug section

--- node/pb-monitor.go
+++ node/pb-monitor.go
@@ -73,21 +73,34 @@ func main() {
 		select {
 		case data := <-chanBeacon:
 
-			fmt.Printf("Time: %s\n BSSID: %s\n SSID: %s\n Transmitter: %v\n Receiver: %v\n Flags: %s\n Proto: %v\n Type: %s\n\n",
-				data.timestamp,
-				data.bssid,
-				data.ssid,
-				data.transmitter,
-				data.receiver,
-				data.pflag,
-				data.proto,
-				data.ptype)
+			if debugOn && data.bssid != "" {
+				fmt.Printf("DEBUG: BEACON PACKET: \n Time: %s\n BSSID: %s\n SSID: %s\n Transmitter: %v\n Receiver: %v\n Flags: %s\n Proto: %v\n Type: %s\n\n",
+					data.timestamp,
+					data.bssid,
+					data.ssid,
+					data.transmitter,
+					data.receiver,
+					data.pflag,
+					data.proto,
+					data.ptype)
+			}
 
 		case data := <-chanProbe:
-			fmt.Printf("Probe: %T", data)
+
+			if debugOn {
+				fmt.Printf("DEBUG: PROBE PACKET: \n Time: %s\n BSSID: %s\n SSID: %s\n Transmitter: %v\n Receiver: %v\n Flags: %s\n Proto: %v\n Type: %s\n\n",
+					data.timestamp,
+					data.bssid,
+					data.ssid,
+					data.transmitter,
+					data.receiver,
+					data.pflag,
+					data.proto,
+					data.ptype)
+			}
 
 		case data := <-chanNone:
-			fmt.Printf("Not interesting: %T", data)
+
 		}
 	}
 }
